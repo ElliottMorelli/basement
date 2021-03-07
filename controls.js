4afa745e25e4b6b750a1d154ccfc86f6f1177a7f
elliott <39219466+ElliottMorelli@users.noreply.github.com>
2021-03-06 20:32:25 -0500

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,48 +1,4 @@
-var audioCtx;
 
-var dragSource;
-
-var source;
-
-async function loadBuffer(bufferURL) {
-    //better to have a try/catch block here, but for simplicity...
-    const response = await fetch(bufferURL);
-    const arrayBuffer = await response.arrayBuffer();
-    const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
-    return audioBuffer;
-  }
-
-  document.addEventListener("DOMContentLoaded", function(event) {
-    playAudio();
-})
-
-
-
-async function playAudio(){
-    audioCtx = new AudioContext();
-
-
-
-    dragSource = await loadBuffer('./you_dont_own_me.mp3');
-    source = audioCtx.createBufferSource();
-    source.buffer = dragSource;
-
-    var lowpass = audioCtx.createBiquadFilter();
-
-    lowpass.type = "lowpass";
-    lowpass.frequency.setValueAtTime(900, audioCtx.currentTime);
-
-    var globalGain = audioCtx.createGain();
-    globalGain.gain.setValueAtTime(0.3, audioCtx.currentTime);
-
-
-    source.connect(lowpass).connect(globalGain).connect(audioCtx.destination); 
-
-    
-    source.start();
-   
-
-}
 
 
 function enterClub(){
